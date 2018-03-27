d8c5490a2bc2241bed4cf9fdcc0d8205fac38cff
Ryan O <windsofryannn@yahoo.com>
2018-03-26 22:40:34 -0700

    ADDED: io, logging, configparser modules Changed: io module used to create messages to be sent to discord client Changed: configparser is now used to load api keys from ini file Changed: Program now uses logging module to show errors rather than a simple print statement

--- Fortneezy-Bot.py
+++ Fortneezy-Bot.py
@@ -1,13 +1,21 @@
 import asyncio
 import requests
+import logging
+import io
+import configparser
 from discord.ext.commands import Bot
 from datetime import datetime
 
-
-TOKEN = 'Client Token'
-HEADERS = {'TRN-Api-Key': 'key'}
-
 client = Bot(command_prefix="!")
+# Load configuration file with API keys
+Config = configparser.ConfigParser()
+Config.read('tokens.ini')
+# Bot Token
+bot_token = Config.get('Tokens', 'bottoken')
+TOKEN = bot_token
+# Tracker Network
+TRN_key = Config.get('Tokens', 'trn-api-key')
+HEADERS = {'TRN-Api-Key': TRN_key}
 
 
 async def pull_stats(user):
@@ -18,10 +26,10 @@ async def pull_stats(user):
     return r.json()
 
 
-async def error_print(error, user):
+async def error_print(user):
     """Prints error when username is not found"""
-    print('Error!\nDate: ', datetime.now())
-    print("KeyError:", error)
+    time = str(datetime.now())
+    logging.error('Username Not Found or no stats for current season, Date: ' + str(time[:-7]))
     await  client.say("Username \"" + user + "\" not found, try again.")
 
 
@@ -31,35 +39,38 @@ async def solo(user: str):
     player_data = await pull_stats(user)
     try:
         solo_L = player_data['stats']['p2']
-        await client.say("Account: " + user + "  Platform: " + player_data['platformNameLong'])
-        await client.say("==Lifetime Solo Stats==")
-        await client.say("Matches Played: " + solo_L['matches']['value'] +
-                         "  ||  Wins: " + solo_L['top1']['value'] +
-                         "  ||  Win Percentage: " + "%.2f" % ((solo_L['top1']['valueInt'] /
-                                                               solo_L['matches']['valueInt']) * 100) + "%")
-        await client.say("Kills: " + solo_L['kills']['value'] +
-                         "  ||  KDR: " + solo_L['kd']['value'] + "%")
-    except KeyError as e:
-        await error_print(e, user)
+        stats = io.StringIO()
+        stats.write("Account: " + user + "  Platform: " + player_data['platformNameLong'])
+        stats.write("\n==Lifetime Solo Stats==")
+        stats.write("\nMatches Played: " + solo_L['matches']['value'] +
+                    "  ||  Wins: " + solo_L['top1']['value'] +
+                    "  ||  Win Percentage: " + "%.2f" % ((solo_L['top1']['valueInt'] /
+                                                          solo_L['matches']['valueInt']) * 100) + "%")
+        stats.write("\nKills: " + solo_L['kills']['value'] +
+                    "  ||  KDR: " + solo_L['kd']['value'] + "%")
+        await client.say(stats.getvalue())
+    except KeyError:
+        await error_print(user)
 
 
 @client.command()
 async def solo_season(user: str):
-    """Display Current Season Solo Stats \ !solo 'username'"""
+    """Display Current Season Solo Stats \ !solo_season 'username'"""
     player_data = await pull_stats(user)
     try:
         solo_s = player_data['stats']['curr_p2']
-        await client.say("Account: " + user + "  Platform: " + player_data['platformNameLong'])
-        await client.say("==Lifetime Solo Stats==")
-        await client.say("Matches Played: " + solo_s['matches']['value'] +
-                         "  ||  Wins: " + solo_s['top1']['value'] +
-                         "  ||  Win Percentage: " + "%.2f" % ((solo_s['top1']['valueInt'] /
-                                                               solo_s['matches'][
-                                                                   'valueInt']) * 100) + "%")
-        await client.say("Kills: " + solo_s['kills']['value'] +
-                         "  ||  KDR: " + solo_s['kd']['value'] + "%")
-    except KeyError as e:
-        await error_print()
+        stats = io.StringIO()
+        stats.write("Account: " + user + "  Platform: " + player_data['platformNameLong'])
+        stats.write("\n==Current Season Solo Stats==")
+        stats.write("\nMatches Played: " + solo_s['matches']['value'] +
+                    "  ||  Wins: " + solo_s['top1']['value'] +
+                    "  ||  Win Percentage: " + "%.2f" % ((solo_s['top1']['valueInt'] /
+                                                          solo_s['matches']['valueInt']) * 100) + "%")
+        stats.write("\nKills: " + solo_s['kills']['value'] +
+                    "  ||  KDR: " + solo_s['kd']['value'] + "%")
+        await client.say(stats.getvalue())
+    except KeyError:
+        await error_print(user)
 
 
 @client.command()
@@ -68,35 +79,38 @@ async def duo(user: str):
     player_data = await pull_stats(user)
     try:
         duo_lifetime = player_data['stats']['p10']
-        await client.say("Account: " + user + "  Platform: " + player_data['platformNameLong'])
-        await client.say("==Lifetime Duo Stats==")
-        await client.say("Matches Played: " + duo_lifetime['matches']['value'] +
-                         "  ||  Wins: " + duo_lifetime['top1']['value'] +
-                         "  ||  Win Percentage: " + "%.2f" % ((duo_lifetime['top1']['valueInt'] /
-                                                               duo_lifetime['matches'][
-                                                                   'valueInt']) * 100) + "%")
-        await client.say("Kills: " + duo_lifetime['kills']['value'] +
-                         "  ||  KDR: " + duo_lifetime['kd']['value'] + "%")
-    except KeyError as e:
-        await error_print(e, user)
+        stats = io.StringIO()
+        stats.write("Account: " + user + "  Platform: " + player_data['platformNameLong'])
+        stats.write("\n==Lifetime Duo Stats==")
+        stats.write("\nMatches Played: " + duo_lifetime['matches']['value'] +
+                    "  ||  Wins: " + duo_lifetime['top1']['value'] +
+                    "  ||  Win Percentage: " + "%.2f" % ((duo_lifetime['top1']['valueInt'] /
+                                                          duo_lifetime['matches']['valueInt']) * 100) + "%")
+        stats.write("\nKills: " + duo_lifetime['kills']['value'] +
+                    "  ||  KDR: " + duo_lifetime['kd']['value'] + "%")
+        await client.say(stats.getvalue())
+    except KeyError:
+        await error_print(user)
 
 
 @client.command()
 async def duo_season(user: str):
-    """Display Current Season Due Stats \ !duo 'username'"""
+    """Display Current Season Due Stats \ !duo_season 'username'"""
     player_data = await pull_stats(user)
     try:
         duo_S = player_data['stats']['curr_p10']
-        await client.say("Account: " + user + "  Platform: " + player_data['platformNameLong'])
-        await client.say("==Lifetime Duo Stats==")
-        await client.say("Matches Played: " + duo_S['matches']['value'] +
-                         "  ||  Wins: " + duo_S['top1']['value'] +
-                         "  ||  Win Percentage: " + "%.2f"
-                         % ((duo_S['top1']['valueInt'] / duo_S['matches']['valueInt']) * 100) + "%")
-        await client.say("Kills: " + duo_S['kills']['value'] +
-                         "  ||  KDR: " + duo_S['kd']['value'] + "%")
-    except KeyError as e:
-        await error_print(e, user)
+        stats = io.StringIO()
+        stats.write("Account: " + user + "  Platform: " + player_data['platformNameLong'])
+        stats.write("\n==Current Season Duo Stats==")
+        stats.write("\nMatches Played: " + duo_S['matches']['value'] +
+                    "  ||  Wins: " + duo_S['top1']['value'] +
+                    "  ||  Win Percentage: " + "%.2f" % ((duo_S['top1']['valueInt'] /
+                                                          duo_S['matches']['valueInt']) * 100) + "%")
+        stats.write("\nKills: " + duo_S['kills']['value'] +
+                    "  ||  KDR: " + duo_S['kd']['value'] + "%")
+        await client.say(stats.getvalue())
+    except KeyError:
+        await error_print(user)
 
 
 @client.command()
@@ -105,38 +119,40 @@ async def squad(user: str):
     player_data = await pull_stats(user)
     try:
         squad_L = player_data['stats']['p9']
-        await client.say("Account: " + user + "  Platform: " + player_data['platformNameLong'])
-        await client.say("==Lifetime Squad Stats==")
-        await client.say("Matches Played: " + squad_L['matches']['value'] +
-                         "  ||  Wins: " + squad_L['top1']['value'] +
-                         "  ||  Win Percentage: " + "%.2f" % ((squad_L['top1']['valueInt'] /
-                                                               squad_L['matches'][
-                                                                   'valueInt']) * 100) + "%")
+        stats = io.StringIO()
+        stats.write("Account: " + user + "  Platform: " + player_data['platformNameLong'])
+        stats.write("\n==Lifetime Squad Stats==")
+        stats.write("\nMatches Played: " + squad_L['matches']['value'] +
+                    "  ||  Wins: " + squad_L['top1']['value'] +
+                    "  ||  Win Percentage: " + "%.2f" % ((squad_L['top1']['valueInt'] /
+                                                          squad_L['matches']['valueInt']) * 100) + "%")
 
-        await client.say("Kills: " + squad_L['kills']['value'] +
-                         "  ||  KDR: " + squad_L['kd']['value'] + "%")
-    except KeyError as e:
-        await error_print(e, user)
+        stats.write("\nKills: " + squad_L['kills']['value'] +
+                    "  ||  KDR: " + squad_L['kd']['value'] + "%")
+        await client.say(stats.getvalue())
+    except KeyError:
+        await error_print(user)
 
 
 @client.command()
 async def squad_season(user: str):
-    """Display Current Squad Stats \ !squad 'username'"""
+    """Display Current Squad Stats \ !squad_season 'username'"""
     player_data = await pull_stats(user)
     try:
         squad_S = player_data['stats']['curr_p9']
-        await client.say("Account: " + user + "  Platform: " + player_data['platformNameLong'])
-        await client.say("==Current Season Squad Stats==")
-        await client.say("Matches Played: " + squad_S['matches']['value'] +
-                         "  ||  Wins: " + squad_S['top1']['value'] +
-                         "  ||  Win Percentage: " + "%.2f" % ((squad_S['top1']['valueInt'] /
-                                                               squad_S['matches'][
-                                                                   'valueInt']) * 100) + "%")
+        stats = io.StringIO()
+        stats.write("\nAccount: " + user + "  Platform: " + player_data['platformNameLong'])
+        stats.write("\n==Current Season Squad Stats==")
+        stats.write("\nMatches Played: " + squad_S['matches']['value'] +
+                    "  ||  Wins: " + squad_S['top1']['value'] +
+                    "  ||  Win Percentage: " + "%.2f" % ((squad_S['top1']['valueInt'] /
+                                                          squad_S['matches']['valueInt']) * 100) + "%")
 
-        await client.say("Kills: " + squad_S['kills']['value'] +
-                         "  ||  KDR: " + squad_S['kd']['value'] + "%")
-    except KeyError as e:
-        await error_print(e, user)
+        stats.write("\nKills: " + squad_S['kills']['value'] +
+                    "  ||  KDR: " + squad_S['kd']['value'] + "%")
+        await client.say(stats.getvalue())
+    except KeyError:
+        await error_print(user)
 
 
 @client.event
@@ -156,5 +172,3 @@ async def list_servers():
 
 client.loop.create_task(list_servers())
 client.run(TOKEN)
-
-
